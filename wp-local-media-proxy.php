772882e917380daecba76ffb4b22652ef0bd274c
Tim Nolte <tnolte@forumone.com>
2022-07-27 15:31:35 -0400

    Fixes Inconsistent URL Replace Handing Due to Missing Slashes
    * Refactors MU Plugin to use a plugin class & OOP.
    * Adds use of trailingslashit to URLs before find/replace use.
    * Updates version bumping to handle setting `@since 0.0.0` references.
    * Fixes text domain settings.
    * Bumps the version number for the next release.

--- wp-local-media-proxy.php
+++ wp-local-media-proxy.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Local Development Media Library Proxy
  * Description: Provides the ability to proxy media library requests to a remote site for local development.
- * Version: 1.0.4
+ * Version: 1.1.0
  * Author: Tim Nolte
  * Author URI: https://www.ndigitals.com
  *
@@ -16,103 +16,69 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Check for remote media URL and add hooks to proxy media library images.
-// Example Configuration "DDEV_USE_REMOTE_MEDIA_URL=https://example.com".
-if ( defined( 'LOCALDEV_USE_REMOTE_MEDIA_URL' ) && ! empty( constant( 'LOCALDEV_USE_REMOTE_MEDIA_URL' ) ) ) {
-	add_filter( 'wp_get_attachment_image_src', 'wplmp_filter_wp_get_attachment_image_src' );
-	add_filter( 'wp_calculate_image_srcset', 'wplmp_filter_wp_calculate_image_srcset' );
-	add_filter( 'wp_get_attachment_url', 'wplmp_filter_wp_get_attachment_url' );
-}
-
 /**
- * Proxy requests for images to a remote URL.
- *
- * @link https://developer.wordpress.org/reference/hooks/wp_get_attachment_image_src/
- *
- * @param array|boolean $image Array of image data, or boolean false if no image is available.
- *
- * @return array|boolean
+ * Require public facing functionality
  */
-function wplmp_filter_wp_get_attachment_image_src( $image = array() ) {
+require_once( plugin_dir_path( __FILE__ ) . 'class-wp-local-media-proxy.php' );
 
-	if ( ! is_array( $image ) || empty( $image ) ) {
-		return $image;
+use Ndigitals\MuPlugin\Wp_Local_Media_Proxy;
+
+// Due to namespace usage and composer package requirements we require PHP >= 7.0.0.
+if ( ! defined( 'PHP_VERSION' ) || ! function_exists( 'version_compare' ) || version_compare( PHP_VERSION, '7.0.0', '<' ) ) {
+
+	// When plugin is used via the WP-CLI print out the text instead of expecting the WordPress Dashboard to be present.
+	if ( class_exists( 'WP_CLI' ) ) {
+
+		WP_CLI::warning( _wplmp_php_version_text() );
+
+	} else {
+
+		// Add Dashboard admin notice for plugin PHP version check failure.
+		add_action( 'admin_notices', '_wplmp_php_version_error' );
+
 	}
+} else {
 
-	$wp_upload_dir = wp_upload_dir();
-	$base_dir      = $wp_upload_dir['basedir'];
-	$base_url      = $wp_upload_dir['baseurl'];
-	$absolute_path = str_replace( $base_url, $base_dir, $image[0] );
-
-	if ( file_exists( $absolute_path ) ) {
-		return $image;
-	}
-
-	$find     = get_home_url();
-	$replace  = LOCALDEV_USE_REMOTE_MEDIA_URL;
-	$image[0] = str_replace( $find, $replace, $image[0] );
-
-	return $image;
+	// Startup the plugin to register hooks.
+	_wplmp_init();
 
 }
 
 /**
- * Proxy srcset requests to a remote URL.
+ * Admin notice for incompatible versions of PHP.
  *
- * @link https://developer.wordpress.org/reference/hooks/wp_calculate_image_srcset/
- *
- * @param array $sources One or more arrays of source data to include in the 'srcset'.
- *
- * @return array
+ * @since 1.1.0
  */
-function wplmp_filter_wp_calculate_image_srcset( $sources = array() ) {
+function _wplmp_php_version_error() {
 
-	if ( is_array( $sources ) && ! is_admin() ) {
-		$wp_upload_dir = wp_upload_dir();
-		$base_dir      = $wp_upload_dir['basedir'];
-		$base_url      = $wp_upload_dir['baseurl'];
-		$find          = get_home_url();
-		$replace       = LOCALDEV_USE_REMOTE_MEDIA_URL;
-		foreach ( $sources as $key => $val ) {
-			$absolute_path = str_replace( $base_url, $base_dir, $val['url'] );
-
-			if ( ! file_exists( $absolute_path ) ) {
-				$val['url']  = str_replace( $find, $replace, $val['url'] );
-				$sources[ $key ] = $val;
-			}
-		}
-	}
-
-	return $sources;
+	printf( '<div class="error"><p>%s</p></div>', esc_html( _wplmp_php_version_text() ) );
 
 }
 
 /**
- * Proxy the attachement URL to a remote site.
+ * String describing the minimum PHP version.
  *
- * @link https://developer.wordpress.org/reference/hooks/wp_get_attachment_url/
+ * @since 1.1.0
  *
- * @param string $url URL for the given attachment.
- *
- * @return string
+ * @return string The localized PHP version requirement message text.
  */
-function wplmp_filter_wp_get_attachment_url( $url = '' ) {
+function _wplmp_php_version_text() {
 
-	if ( is_admin() ) {
-		return $url;
-	}
+	return __( 'Local Media Proxy MU Plugin error: Your version of PHP is too old to run this MU Plugin. You must be running PHP 7.0 or higher.', 'wp-local-media-proxy' );
 
-	$wp_upload_dir = wp_upload_dir();
-	$base_dir      = $wp_upload_dir['basedir'];
-	$base_url      = $wp_upload_dir['baseurl'];
-	$find          = get_home_url();
-	$replace       = LOCALDEV_USE_REMOTE_MEDIA_URL;
-	$absolute_path = str_replace( $base_url, $base_dir, $url );
+}
 
-	if ( ! file_exists( $absolute_path ) ) {
-		$url = str_replace( $find, $replace, $url );
-	}
+/**
+ * Intializes the main plugin global instance.
+ *
+ * @since 1.1.0
+ *
+ * @return Ndigitals\MuPlugin\Wp_Local_Media_Proxy The single Ndigitals\MuPlugin\Wp_Local_Media_Proxy instance.
+ */
+function _wplmp_init() {
 
-	return $url;
+	$muplugin = Wp_Local_Media_Proxy::get_instance();
+
+	return $muplugin;
 
 }
